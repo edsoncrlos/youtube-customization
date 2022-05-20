0ad1c2242908ca48c07ff0df4c0f43b279ba713d
Edson Carlos <edsoncarlos.dev@gmail.com>
2022-05-20 11:52:11 -0300

    adding transition to video

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-ytVideo = {
-    open() {
-        document.querySelector('.yt-background').classList.add('on')
+let show = true;
+
+Utils = {
+    toggle () {    
+        const background = document.
+        querySelector('.yt-video-background')
+        
+        background.classList.toggle("on", show) 
+        show = !show;
+    }
+}
+
+const Form = {
+    "url-link": document.querySelector('input#url-link'),
+    "select-width": document.querySelector('input#select-width'),
+
+    getValues() {
+        return {
+            "url-link": Form["url-link"].value,
+            "select-width": Form["select-width"].value,
+        }
+    },
+
+    submit(event) {
+        event.preventDefault()
+
+        try {
+            Utils.toggle()
+            
+        } catch (error) {
+            alert(error.message)
+        }
+        console.log(Form.getValues())
     }
 }

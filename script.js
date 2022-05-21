2e80602378a861044784872eff99a109d0f2d9f2
Edson Carlos <edsoncarlos.dev@gmail.com>
2022-05-21 20:06:54 -0300

    add Object Video

--- script.js
+++ script.js
@@ -10,6 +10,35 @@ Utils = {
     },
 }
 
+const Video = {
+    addVideo() {
+        const {urlLink, width} = Form.getValues();
+        const height = width*9/16;
+        const link = Video.getLink(urlLink);
+        const ytVideo = document.querySelector('.yt-video-background')
+        
+        console.log(link)
+        ytVideo.innerHTML = `
+            <iframe width="${width}" height="${height}" src="https://www.youtube.com/embed/${link}?start=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        `
+    },
+
+    getLink(urlLink) {
+        let link;
+    
+        if (urlLink.indexOf("watch") != -1) {
+            const indexEndLink = urlLink.indexOf("&") == -1 ? urlLink.length : urlLink.indexOf("&");
+
+            link = urlLink.slice(urlLink.indexOf("=")+1, indexEndLink)
+        } else if (urlLink.indexOf("youtu.be") != -1) {
+            const indexStart = urlLink.indexOf("youtu.be")+9;
+
+            link = urlLink.slice(indexStart);
+        }
+        return link;
+    }
+}
+
 const Form = {
     urlLink: document.querySelector('input#url-link'),
     width: document.querySelector('input#select-width'),
@@ -31,34 +60,14 @@ const Form = {
 
     submit(event) {
         event.preventDefault()
-
+        
         try {
             Utils.toggle()
             Form.validateFields()
+            Video.addVideo()
         } catch (error) {
             alert(error.message)
         }
         console.log(Form.getValues())
     }
 }
-
-const Video = {
-    addVideo() {
-        const {urlLink, width} = Form.getValues();
-        const height = width*9/16;
-        console.log(width + " " + height)
-        const ytVideo = document.querySelector('.yt-video-background')
-        
-        console.log(urlLink)
-
-        let ytURLIndex = String(urlLink.indexOf("watch"));
-        console.log(ytURLIndex)
-        console.log(urlLink[24])
-
-        ytVideo.innerHTML = `
-            <iframe width="${width}" height="${height}" src="https://www.youtube.com/embed/ngVU74daJ8Y?start=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        `
-    }
-}
-
-Video.addVideo()

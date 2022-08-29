a3e9ace814bae83a6403d95ad3f575f8b5e03a98
edson <edsoncarlos.dev@gmail.com>
2022-08-29 17:17:27 -0300

    refactor: select string path with regular structure

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,22 +2,32 @@ import { Tumbnails } from './tumbnails.js';
 import { ytVideo, Controler } from './video.js';
 import { Form } from './form.js';
 
-const route = document.URL.split('/')[3];
-if (route === '') {
-	Controler.thereVideo();
+try {
+	const regexURLLastPath = /\/[\w-]*($|(?=\?))/g;
+	const path = document.URL.match(regexURLLastPath);
 
-} else if (route.indexOf('tumbnails') !== -1) {
-	Tumbnails.showTumbnails();
-	Controler.setLinkReturn('/tumbnails');
-	Tumbnails.buttonToggleExclusion.addEventListener('click', Tumbnails.toggleMenuForExclusion);
-
-
-} else if (route.indexOf('form') !== -1) {
-	Controler.setLinkReturn('/form');
-	const form = document.querySelector('#form form');
-	form.addEventListener('submit', (event) => Form.submit(event));
-	
-} else if (route.indexOf('yt-video') !== -1) {
-	ytVideo.addVideo();
-	ytVideo.returnLink();
+	if (path != null) {
+		if (path[0] === '/') {
+			Controler.thereVideo();
+		
+		} else if (path[0] ==='/tumbnails') {
+			Tumbnails.showTumbnails();
+			Controler.setLinkReturn('/tumbnails');
+			Tumbnails.buttonToggleExclusion.addEventListener('click', Tumbnails.toggleMenuForExclusion);
+		
+		
+		} else if (path[0] === '/form') {
+			Controler.setLinkReturn('/form');
+			const form = document.querySelector('#form form');
+			form.addEventListener('submit', (event) => Form.submit(event));
+			
+		} else if (path[0] === '/yt-video') {
+			ytVideo.addVideo();
+			ytVideo.returnLink();
+		}		
+	} else {
+		throw new Error('invalid URL');
+	}
+} catch (e) {
+	console.log(e);
 }
